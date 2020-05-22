df2313992b1b2bde0ef420a9c1292be3ea96c32c
bird-grid2 <bird2.one.regend@gmail.com>
2020-05-23 01:44:01 +0900

    modified_deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 server '54.250.23.227', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # server-based syntax
 # ======================
