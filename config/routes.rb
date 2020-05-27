c5bdcda29366208037203b7871201f50e1ed5e08
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-05-27 17:30:25 +0900

    create-purchase-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'purchases#index'
+  resources :purchases
+  resources :addresses
+  get 'purchases/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
