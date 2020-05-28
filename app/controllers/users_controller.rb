b0169f8ad1522a7319c99c9f387f932485297afa
bird-grid2 <bird2.one.regend@gmail.com>
2020-05-28 23:24:15 +0900

    modified_mypage_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
-def index
-end
+  def index
+  end
 
-def edit
-  @user = User.find(params[:id])
-end
+  def edit
+  end
 
-def destroy
-end
+  def destroy
+  end
 
+  private
+
+  def user_params
+  params.require(:user).permit(:nickname, :email)
+  end
 end
