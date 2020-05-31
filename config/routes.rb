e147cad293887199cdf381fdc3f084046e91a561
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-05-31 18:55:40 +0900

    change-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'addresses/index'
-  get 'orders/index'
  root 'orders#index'
 
  resources :items, only: [ :index, :show , :new ]
  resources :users, only: [ :index, :edit, :update, :show ]
  resources :orders, only: [ :index]
+ resources :addresses, only: [ :index]
+
 end
 
