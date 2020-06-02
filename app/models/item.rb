500fa381f14dffe8f6e391ceb2fdf41280acac8a
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-02 22:57:59 +0900

    create_item_image_table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
-  
+  belongs_to :user
+  has_one :order
+  belongs_to :category
+  has_many :item_images dependent: :destroy
+  has_many :comments dependent: :destroy
+  has_many :likes dependent: :destroy
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_period
+
 end
