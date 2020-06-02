becf3026ea5c8ca8d8df8b7019d6f0b15bfff51b
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-06-02 17:42:35 +0900

    delete-test-files

--- test/controllers/shipping_address_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ShippingAddressControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get shipping_address_index_url
-    assert_response :success
-  end
-
-end
