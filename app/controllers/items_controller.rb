86df429b6c5b60392ac363081723a5f70369e751
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-04 22:41:24 +0900

    Create instance variable
    Create instance variables so that we can get the categoryname of the product at items#show action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,11 @@
 class ItemsController < ApplicationController
 
-
-  
   def index
-
   end
   
   def show
-    
+    @item = Item.find(params[:id])
+    @category = Category.find(@item.category_id)
   end
 
   def new
