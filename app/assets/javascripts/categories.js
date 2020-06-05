3bc8b36e6d53445a4f622efe9b0cac8f2fdd2224
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-05 20:51:45 +0900

    Describe the <option> code to put the category data

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,3 +1,7 @@
 $(function() {
+  function appendOption(category){
+    var html = `<option value="${category.id}"  data-category="${category.id}">${category.name}</option>`;
+    return html;
+  }
 });
 
