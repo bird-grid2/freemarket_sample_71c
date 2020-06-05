7a8a203644a58612af76a10468bf7d35eab61c9e
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-05 20:55:45 +0900

    Describe the process when the child category is selected

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -52,5 +52,31 @@ $(function() {
       $('#grandchildren_options').remove();
     }
   });
+  $('.category').on('change', '#child_category', function(){
+    var child_category_id = $('#child_category option:selected').data('category');
+    if (child_category_id != ""){
+      $.ajax({
+        url: '/items/category/get_grandchildren_categories',
+        type: 'GET',
+        data: { child_id: child_category_id },
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        if (grandchildren.length != 0) {
+          $('#grandchildren_options').remove();
+          var insertHTML = '';
+          grandchildren.forEach(function(grandchild){
+            insertHTML += appendOption(grandchild);
+          });
+          appendGrandchidrenBox(insertHTML);
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
+      $('#grandchildren_options').remove();
+    }
+  });
 });
 
