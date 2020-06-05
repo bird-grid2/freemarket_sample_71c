b20ce044ca1b366eaff1e65baa543ce2649d93a3
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-05 20:53:17 +0900

    Discribe HTML for the Child category

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -3,5 +3,16 @@ $(function() {
     var html = `<option value="${category.id}"  data-category="${category.id}">${category.name}</option>`;
     return html;
   }
+  function appendChidrenBox(insertHTML){
+    var childSelectHtml = '';
+    childSelectHtml = `<div id= 'children_options'>
+                        <i class='fas fa-chevron-down'></i>
+                        <select class="category__form--child" id="child_category" name="item[child_id]">
+                          <option value="---" data-category="">選択して下さい</option>
+                          ${insertHTML}
+                        <select>
+                      </div>`;
+    $('.category__form').append(childSelectHtml);
+  }
 });
 
