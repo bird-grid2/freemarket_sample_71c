17b455e0f13109293f1d93327846949260a597ed
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-05 20:53:52 +0900

    Discribe HTML for the Grandchild category

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -14,5 +14,16 @@ $(function() {
                       </div>`;
     $('.category__form').append(childSelectHtml);
   }
+  function appendGrandchidrenBox(insertHTML){
+    var grandchildSelectHtml = '';
+    grandchildSelectHtml = `<div id= 'grandchildren_options'>
+                              <i class='fas fa-chevron-down'></i>
+                              <select class="category__form--grandchild" id="grandchild_category" name="item[category_id]">
+                                <option value="---" data-category="">選択して下さい</option>
+                                ${insertHTML}
+                              </select>
+                            </div>`;
+    $('.category__form').append(grandchildSelectHtml);
+  }
 });
 
