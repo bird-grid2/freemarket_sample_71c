d1d90479ed2064ff7e141f31348cfb54f2f7b87d
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-05 20:40:22 +0900

    Describe the route from ajax
    Used in the category selection form when listing products.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
  resources :items, only: [ :show , :new ]
  resources :users, only: [ :index, :edit, :update, :show]
 
+ resources :items do 
+  collection do
+    get 'category/get_children_categories', to: 'items#get_children_categories', defaults: { format: 'json' }
+    get 'category/get_grandchildren_categories', to: 'items#get_grandchildren_categories', defaults: { format: 'json' }
+  end
+end
+
 end
