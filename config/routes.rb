893fed071fdffd0ca4ddf2c0f31d092408223e77
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-06-06 14:36:39 +0900

    Change the nesting position of routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
- resources :items, only: [ :show , :new, :create]
+  resources :items, only: [ :show , :new, :create] do
+    collection do
+      get 'category/get_children_categories', to: 'items#get_children_categories', defaults: { format: 'json' }
+      get 'category/get_grandchildren_categories', to: 'items#get_grandchildren_categories', defaults: { format: 'json' }
+    end
+  end
  resources :users, only: [ :index, :edit, :update, :show]
  resources :orders, only: [ :index]
  resources :shipping_addresses, only: [ :index]
-
- resources :items do 
-  collection do
-    get 'category/get_children_categories', to: 'items#get_children_categories', defaults: { format: 'json' }
-    get 'category/get_grandchildren_categories', to: 'items#get_grandchildren_categories', defaults: { format: 'json' }
-  end
-end
 
 end
