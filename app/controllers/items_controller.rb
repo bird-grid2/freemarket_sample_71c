56622583e89a1ad8572a4e0f9790de1188600e3d
ryota-shimizu0703 <62129752+ryota-shimizu0703@users.noreply.github.com>
2020-06-08 15:15:15 +0900

    server-side-item-detail
    商品詳細表示ページ条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,19 @@
 class ItemsController < ApplicationController
 
-
-  
   def index
 
   end
   
   def show
-    
+    @item = Item.find(params[:id])
   end
 
   def new
+  end
+
+  private
+  def item_params
+    params.require(:item).premit(:name, :description, :brand, :price).merge(user_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_171602) do
+ActiveRecord::Schema.define(version: 2020_06_04_020347) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "brand"
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_05_22_171602) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
