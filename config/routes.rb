d3e07c2b1d2e92a51f4bcc755d6b40aa2265d33b
ryota-shimizu0703 <62129752+ryota-shimizu0703@users.noreply.github.com>
2020-06-11 22:42:53 +0900

    server-side-item-detail
    root修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
- resources :items, only: [ :show , :new ]
+ resources :items, only: [ :show , :new, :destroy, :edit, :update ]
  resources :users, only: [ :index, :edit, :update, :show]
 
 end
