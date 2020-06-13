8cd95da24f482d63ccb1ce91b3838c773eed0902
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-13 15:44:59 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,10 @@ class UsersController < ApplicationController
   def index
   end
 
-  def new
-    @user = User.new
-  end
-
   def show
   end
 
   def edit
-  end
-
-  def create
-    @user = User.new(configure_permitted_parameters)
-    if @user.save
-      redirect_to addresses_path
-    else
-      reder :new      
-    end
   end
 
   private
