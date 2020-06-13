91db4f1659362271dac1e0da58b95ad3f38a3ac1
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-13 14:55:50 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(sign_up_params)
+    @user = User.new(configure_sign_up_params)
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
       render :new and return
     end
-   
-    session["devise.regist_data"] = {user: @user.attributes}
-    session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @address = @user.build_address
-    render :new_address
-   end
+  end
 
   private
 
