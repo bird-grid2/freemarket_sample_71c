14c931362b10af7a57fe646f47748d0f5499bb1a
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-14 00:09:56 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,23 @@ ActiveRecord::Schema.define(version: 2020_06_03_184944) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "post_code", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building"
+    t.string "phone_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shipping_addresses_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -71,4 +88,5 @@ ActiveRecord::Schema.define(version: 2020_06_03_184944) do
   add_foreign_key "item_images", "items"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
+  add_foreign_key "shipping_addresses", "users"
 end
