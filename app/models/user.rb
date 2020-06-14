ea81794a6485aa6d78c726a6e5df53706d50ce9b
ryota-shimizu0703 <62129752+ryota-shimizu0703@users.noreply.github.com>
2020-06-14 18:45:21 +0900

    delete-user.rb
    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true,  uniqueness: true
-
   has_many :items, dependent: :destroy
 
   validates :birthday, presence: true
