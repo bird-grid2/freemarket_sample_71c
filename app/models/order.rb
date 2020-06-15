2cf501de04de4d32814c23668246b758ff4c1886
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-06-15 20:25:59 +0900

    delete-order.rb/create-order.rb

--- app/models/order.rb
+++
@@ -1,8 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
-
-  validates :shipping_address, presence: true  
-  validates :cards, presence: true
-
-end
--- db/migrate/20200531033315_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.references :user_id, type: :bigint, null: false, foreign_key: true
-      t.references :item, type: :bigint, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
