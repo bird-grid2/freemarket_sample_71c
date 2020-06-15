60a6cb38cf0fccfcea3ff10c150740ed6638304f
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-06-15 19:55:35 +0900

    change-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'orders#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  root 'items#index'
 
- resources :items, only: [ :show , :new ]
- resources :users, only: [ :index, :edit, :update, :show ]
- resources :orders, only: [ :index] do
-  collection do
-    get 'confirm', to: 'orders#confirm'
-    post 'pay', to: 'orders#pay'
-    get 'done', to: 'orders#done'
+  resources :items, only: [ :show , :new, :create] do
+    collection do
+      get 'category/get_children_categories', to: 'items#get_children_categories', defaults: { format: 'json' }
+      get 'category/get_grandchildren_categories', to: 'items#get_grandchildren_categories', defaults: { format: 'json' }
+    end
   end
- end
+
+ resources :users, only: [ :index, :edit, :update, :show]
+ resources :orders, only: [ :index]
  resources :shipping_addresses, only: [ :index]
- resources :cards, only: [ :show , :new ] do
-  collection do
-    post 'show', to: 'cards#show'
-    post 'pay', to: 'cards#pay'
-    post 'delete', to: 'cards#delete'
-  end
- end
+
 end
