292bd60898c88abde3af68894781fe295b0f1960
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-17 21:59:44 +0900

    modhified_edit.html.haml

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -4,34 +4,19 @@ $(document).on('turbolinks:load',()=> {
 
   for ( let i = 0; i < num; i++){
 
-    let src = $('#image-box').find('img').data('index', i).attr("src");
-
-    let name = num + '.jpg';
-
-    let html =`<div class='item-image' data-image="${name}">
-                    <div class=' item-image__content'>
-                      <div class='item-image__content--icon'>
-                        <img src=${src} width="70" height="70" >
-                      </div>
-                    </div>
-                    <div class='item-image__operation'>
-                      <div class='item-image__operation--delete'>削除</div>
-                    </div>
-                  </div>`
-
-    $('#image-box__container').before(html);
-
     $('#image-box__container').attr('class', `item-num-${num}`)
 
+    $('.hidden-destroy').hide();
     
   };
 });
 
+
 $(document).on("click", '.item-image__operation--delete', ()=> {
 
-  var target_image = $(this).parent().parent()
+  let target_image = $(this).parent().parent()
 
-  var target_name = $(target_image).data('image')
+  let target_name = $(target_image).data('image')
 
   if(file_field.files.length==1){
 
@@ -52,11 +37,10 @@ $(document).on("click", '.item-image__operation--delete', ()=> {
   target_image.remove()
 
   var num = $('.item-image').length
+
   $('#image-box__container').show()
   $('#image-box__container').attr('class', `item-num-${num}`)
-})
-
-
+});
 
 
 window.onload = function(e){
@@ -65,19 +49,19 @@ window.onload = function(e){
 
   dropArea.addEventListener("dragover", function(e){
     e.preventDefault();
-
     $(this).children('#image-box__container').css({'border': '1px solid rgb(204, 204, 204)','box-shadow': '0px 0px 4px'})
   },false);
 
   dropArea.addEventListener("dragleave", function(e){
     e.preventDefault();
-
     $(this).children('#image-box__container').css({'border': '1px dashed rgb(204, 204, 204)','box-shadow': '0px 0px 0px'})      
   },false);
 
   dropArea.addEventListener("drop", function(e) {
     e.preventDefault();
+    
     $(this).children('#image-box__container').css({'border': '1px dashed rgb(204, 204, 204)','box-shadow': '0px 0px 0px'});
+    
     var files = e.dataTransfer.files;
 
     $.each(files, function(i,file){
@@ -98,10 +82,10 @@ window.onload = function(e){
       fileReader.onload = function() {
 
         var src = fileReader.result
-        var html =`<div class='item-image' data-image="${file.name}">
+        var html =`<div class='item-image'>
                     <div class=' item-image__content'>
                       <div class='item-image__content--icon'>
-                        <img src=${src} width="114" height="80" >
+                        <img src=${src} width="70" height="70" >
                       </div>
                     </div>
                     <div class='item-image__operation'>
@@ -116,3 +100,22 @@ window.onload = function(e){
     })
   })
 }
+
+$(document).on("click", '.item-image__edit--delete', ()=> {
+
+  const targetIndex = $(this).parent().data('index');
+
+  const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+
+  if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+    $(this).parent().remove();
+    
+    $(`img[data-index="${targetIndex}"]`).remove();
+
+  var num = $('.item-image').length
+  
+  $('#image-box__container').show()
+  $('#image-box__container').attr('class', `item-num-${num}`)
+
+});
