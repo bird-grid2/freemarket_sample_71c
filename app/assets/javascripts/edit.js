6e0f173ed7b24925140c85810fd60844099b61be
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-19 22:49:40 +0900

    modified_edit.js_2

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -151,7 +151,7 @@ $(window).on("turbolinks:load", function() {
     }
   });
 
-  $('.edit_item').on('submit', function(e){
+  $('.exhibit').on('submit', function(e){
     // 通常のsubmitイベントを止める
     e.preventDefault();
     // images以外のform情報をformDataに追加
