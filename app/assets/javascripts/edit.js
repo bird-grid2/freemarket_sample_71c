a235d924329dba36021fcbc9705ee3e4dcd34c5c
bird-grid2 <bird2.one.regend@gmail.com>
2020-06-30 20:08:19 +0900

    Update edit.js

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,4 +1,4 @@
-$(window).onload = function() {
+window.onload = function() {
   if (document.URL.match(/edit/)) {
     var dropzone = $(".upload-images-edit");
     var input_area = $(".input-area");
