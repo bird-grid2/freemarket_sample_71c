f1763e67babed61c8ed2baffa6ea25c8cab57662
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-01 01:28:55 +0900

    modified_tables

--- db/migrate/20200531031234_create_cards.rb
+++ db/migrate/20200531031234_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[5.2]
     create_table :cards do |t|
       t.string :customer_token, null: false
       t.references :user, foreign_key: true
+      
       t.timestamps
     end
   end
