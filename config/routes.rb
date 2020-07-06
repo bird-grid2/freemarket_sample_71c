63741877c8b38afa35c0518d618aba8834efe128
KAHORIK-tech <63528317+KAHORIK-tech@users.noreply.github.com>
2020-07-06 22:35:14 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-  resources :items, only: [ :show , :new, :create, :update] do
+  resources :items do
     collection do
       get 'category/get_children_categories', to: 'items#get_children_categories', defaults: { format: 'json' }
       get 'category/get_grandchildren_categories', to: 'items#get_grandchildren_categories', defaults: { format: 'json' }
