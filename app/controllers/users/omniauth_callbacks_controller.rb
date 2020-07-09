d0126e84fdf0a4e896a5727dd298510d43b33e41
nagaokak68 <62579941+nagaokak68@users.noreply.github.com>
2020-07-09 11:52:12 +0900

    Omniauth_testcode

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
   def facebook
     authorization
@@ -23,36 +19,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = sns_info[:user]
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, event: :authentication
     else
-      # binding.pry
       @sns_id = sns_info[:sns].id
       session["sns_id"] = @sns_id
       render template: 'devise/registrations/new'
     end
   end
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
