e97e80767645243ee050874b27300a922c4b7025
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-07-11 16:19:10 +0900

    update-item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images
 
   belongs_to :category
-  #validates :category_id, presence: true
+  validates :category_id, presence: true
   belongs_to_active_hash :postage
 
   belongs_to :seller, class_name: "User", :foreign_key => 'seller_id'
