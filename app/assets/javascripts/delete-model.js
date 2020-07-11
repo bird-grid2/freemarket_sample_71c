083324fbb04e63fea94cc58a0c6f8332a8159209
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-12 00:55:00 +0900

    modified_delete_fuction

--- app/assets/javascripts/delete-model.js
+++ app/assets/javascripts/delete-model.js
@@ -1,18 +1,15 @@
-$(window).on('turbolinks:load', function() {
-  if (document.URL.match(/items/)) { 
-    $(document).ready( function(e) {
-        let modal_open = $("#modal-open-btn");
-        modal_open.onclick = function () {
-          $('#overlay').fadeIn();
-          $('#modal-close-btn').onclick = function () {
-            $('#overlay').fadeOut();
-          };
-          $("#delete-comformation-btn").onclick = function () {
-            $("#item-delete-btn").click();
-          };
-        };
-      },
-      false
-    );
-  };
-});
+document.addEventListener(
+  "DOMContentLoaded", e => {
+    let modal_open = document.getElementById("modal-open-btn");
+    modal_open.onclick = function () {
+      $('#overlay').fadeIn();
+      document.getElementById('modal-close-btn').onclick = function () {
+        $('#overlay').fadeOut();
+      };
+      document.getElementById("delete-comformation-btn").onclick = function () {
+        document.getElementById("item-delete-btn").click();
+      };
+    };
+  },
+  false
+);
