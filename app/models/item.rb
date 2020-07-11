535c98ad94c52a76b3b6d3078a421b272bb439b3
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-07-11 17:19:06 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller, class_name: "User", :foreign_key => 'seller_id'
   belongs_to :buyer, class_name: "User", :foreign_key => 'buyer_id', optional: true
-  belongs_to :user
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
   belongs_to_active_hash :prefecture
@@ -23,8 +22,7 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
-  has_many   :item_images, dependent: :destroy, inverse_of: :item
-  has_many   :item_images, dependent: :destroy
+  has_many :item_images, dependent: :destroy, inverse_of: :item
 
   accepts_nested_attributes_for :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true
