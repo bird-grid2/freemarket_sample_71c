f42e52ccc852b492915954348f45f930cf50db37
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-11 19:20:34 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,6 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User", :foreign_key => 'seller_id'
   belongs_to :buyer, class_name: "User", :foreign_key => 'buyer_id', optional: true
 
-
-  belongs_to :user
   has_many   :item_images, dependent: :destroy, inverse_of: :item
   accepts_nested_attributes_for :item_images, allow_destroy: true
   has_many :comments, dependent: :destroy
