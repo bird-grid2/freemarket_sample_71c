b4d35c472fa98d589250c7f2d33f7071c035a96a
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-07-11 16:01:11 +0900

    update-items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: [:index, :new, :create, :get_children_categories, :get_grandchildren_categories, :search]
-
   require 'payjp'
+
+  before_action :set_item, except: [:index, :new, :create, :get_children_categories, :get_grandchildren_categories, :search]
   before_action :set_card, except: [:index, :show]
 
   def index
@@ -127,8 +127,6 @@ class ItemsController < ApplicationController
     @sold_item = Item.find(params[:id])
     @sold_item.update_attribute(:buyer_id, current_user.id)
   end
-
-  private
   
   def edit
     gon.item = @item
