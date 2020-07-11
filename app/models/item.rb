eb3171b3e8e45cd0455ae4275ee4aaae8a480c03
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-07-11 16:40:18 +0900

    update-items-controller.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,35 +1,33 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  has_many   :item_images, dependent: :destroy
-  accepts_nested_attributes_for :item_images
 
   belongs_to :category
-  validates :category_id, presence: true
-  belongs_to_active_hash :postage
-
   belongs_to :seller, class_name: "User", :foreign_key => 'seller_id'
   belongs_to :buyer, class_name: "User", :foreign_key => 'buyer_id', optional: true
-
-  validates :postage_id, :price, :item_images, presence: true
-
   belongs_to :user
-  has_many   :item_images, dependent: :destroy, inverse_of: :item
-  accepts_nested_attributes_for :item_images, allow_destroy: true
-  has_many :comments, dependent: :destroy
-
-  has_many :likes, dependent: :destroy
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_period
   belongs_to_active_hash :shipping_method
+  belongs_to_active_hash :postage
+
+  validates :category_id, presence: true
+  validates :postage_id, :price, :item_images, presence: true
   validates :category_id, :name, :description, :condition_id, :postage_id, :prefecture_id, :preparation_period_id, :price, :shipping_method_id, :item_images,  presence: true
   validates :name, :brand, length: {maximum: 15 }
   validates :description, length: {maximum: 200 }
   validates :price, numericality:  { only_integer: true }
 
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :users, through: :likes
+  has_many   :item_images, dependent: :destroy, inverse_of: :item
+  has_many   :item_images, dependent: :destroy
+
+  accepts_nested_attributes_for :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
 
   def already_liked(user_id)
     likes.find_by(user_id: user_id)
