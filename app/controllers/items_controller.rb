ca5679d89f8b42c6d93665d85ff9f0b3b2e746b6
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-07-11 13:06:27 +0900

    change-items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -75,7 +75,7 @@ class ItemsController < ApplicationController
   def purchase
     if user_signed_in?
       @images = @item.item_images
-      @shipping_address = ShippingAddress.where(user_id: current_user.id).first
+      @shipping_address = ShippingAddress.find_by(user_id: current_user.id)
       @condition = @card.blank? || @shipping_address.blank? || user_signed_in? && current_user.id == @item.seller_id || @item.buyer_id.present?
       #購入ボタンが押せない条件
 
@@ -101,7 +101,7 @@ class ItemsController < ApplicationController
   end
 
   def confirm
-    @card = Card.where(user_id: current_user.id).first                             #テーブルからpayjpの顧客IDを検索
+    @card = Card.find_by(user_id: current_user.id)                             #テーブルからpayjpの顧客IDを検索
     if @card.blank?                                                  #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
@@ -112,7 +112,7 @@ class ItemsController < ApplicationController
   end
 
   def pay
-    @card = Card.where(user_id: current_user.id).first
+    @card = Card.find_by(user_id: current_user.id)
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => @item.price,                                         #支払金額を入力
@@ -139,7 +139,7 @@ class ItemsController < ApplicationController
   end
 
   def set_card
-    @card = Card.where(user_id: current_user.id).first
+    @card = Card.find_by(user_id: current_user.id)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  #devise_for :users, controllers: {
-    #omniauth_callbacks: 'users/omniauth_callbacks',
-    #registrations: 'users/registrations'
-  #}
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
