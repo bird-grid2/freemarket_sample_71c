da75a47c1ca038b7392f6663646b137cb95b5c3b
KAHORIK-tech <akikutu75@gmail.com>
2020-07-13 21:24:17 +0900

    Revert "Revert "master merge""
    This reverts commit 6a4a6784eb5d4ee04d25d3c6b2f2da48a880745a.

---
+++ app/assets/javascripts/delete-modal.js
@@ -0,0 +1,13 @@
+document.addEventListener("turbolinks:load", ()=> {
+  if(document.URL.match(/items/)) {
+    $("#modal-open-btn")[0].onclick =()=> {
+      $('#overlay').fadeIn();
+      $('#modal-close-btn')[0].onclick =()=> {
+        $('#overlay').fadeOut();
+      };
+      $("#delete-comformation-btn")[0].onclick =()=> {
+        $("#item-delete-btn")[0].click();
+      };
+    };
+  };
+});
