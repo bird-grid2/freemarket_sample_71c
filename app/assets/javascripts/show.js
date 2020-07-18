deb00c7b9757255bd1348e4b3afe30223287d8b8
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-18 21:34:43 +0900

    updated_item_show_function

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,8 @@
+$(document).on("turbolinks:load", function() {
+  $('.mini_image').on('click', function(){
+      target = $(this).clone(true)[0]
+      $('.item-content__photo--top img').remove()
+      $('.item-content__photo--top').append(target)
+      $('.item-content__photo--top img').removeClass('mini_image')
+  });
+});
