0e2d53ccc242d9afe62aba14ab941b58d92d81f5
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-07-18 20:58:39 +0900

    Write test code

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,13 +4,13 @@ FactoryBot.define do
     name                  {"picture"}
     description           {"morinoe"}
 
-    after(:build) do |item|
-      parent = create(:category, name: "親", parent_id: nil)
-      child = create(:category, name: "子", parent_id: parent.id)
-      grand_child = create(:category, name: "孫", parent_id: child.id)
-      item.category_id = grand_child.id
-    end
-
+    #after(:build) do |item|
+      #parent = create(:category, name: "親", parent_id: nil)
+      #child = create(:category, name: "子", parent_id: parent.id)
+      #grand_child = create(:category, name: "孫", parent_id: child.id)
+      #item.category_id = grand_child.id
+    #end
+    category_id           {3}
     seller_id             {1}
     buyer_id              {2}
     brand                 {"france"}
