b0ae6b5ec425447ac4836c5378629b097ff73141
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-18 21:49:17 +0900

    Update show.js

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,8 +1,10 @@
 $(document).on("turbolinks:load", function() {
-  $('.mini_image').on('click', function(){
-      target = $(this).clone(true)[0]
-      $('.item-content__photo--top img').remove()
-      $('.item-content__photo--top').append(target)
-      $('.item-content__photo--top img').removeClass('mini_image')
-  });
+  if (document.URL.match(/item/)) {
+    $('.mini_image').on('click', function(){
+        target = $(this).clone(true)[0]
+        $('.item-content__photo--top img').remove()
+        $('.item-content__photo--top').append(target)
+        $('.item-content__photo--top img').removeClass('mini_image')
+    });
+  };
 });
